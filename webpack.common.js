464c29553eb3faaee590178c618296cab58b27b6
JohnnyMu <404345032@qq.com>
2020-12-09 22:34:20 +0800

    init blog

---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: "./src/index.js",
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            filename: 'index.html',
+            title: 'Production',
+            inject: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/, // jsx/js文件的正则
+                exclude: /node_modules/, // 排除 node_modules 文件夹
+                use: {
+                    // loader 是 babel
+                    loader: 'babel-loader',
+                    options: {
+                        // babel 转义的配置选项
+                        babelrc: false,
+                        presets: [
+                            // 添加 preset-react
+                            require.resolve('@babel/preset-react'),
+                            [require.resolve('@babel/preset-env'), {modules: false}]
+                        ],
+                        cacheDirectory: true
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: ['file-loader']
+            }
+        ]
+    },
+    optimization: {
+        runtimeChunk: 'single', // 当依赖的异步加载模块改变时，不会影响调用他的主模块build的hash值
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    minChunks: 2
+                }
+            }
+        }
+    },
+    resolve: {
+        extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx']
+    }
+};
